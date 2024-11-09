9ebcbe6d868b7b1152fd185d68b1fd0fa7d30c95
su1y68 <1477690040@qq.com>
2024-11-10 01:50:12 +0800

    Update bing脚本.lua

--- bing脚本.lua
+++ bing脚本.lua
@@ -47,7 +47,6 @@ local win = ui:new("bing脚本")
 local UITab1 = win:Tab("『信息』",'7734068321')
 
 local about = UITab1:section("『作者信息』",false)
-
 about:Label("你的账号年龄:"..player.AccountAge.."天")
 about:Label("你现在的服务器id:"..game.GameId)
 about:Label("你的用户ID:"..game.Players.LocalPlayer.UserId)
