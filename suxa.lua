536e270670de734c9c6c984095dd391f54405e86
Suxa <1477690040@qq.com>
2024-12-08 21:11:36 +0800

    Update suxa.lua

--- suxa.lua
+++ suxa.lua
@@ -29,20 +29,11 @@ local Window = Rayfield:CreateWindow({
 local MainTab = Window:CreateTab("玩家信息", nil) -- Title, Image
 
 local MainSection = MainTab:CreateSection("服务器👇")
-
-local MainSection = MainTab:CreateSection("你现在的服务器id:"..game.GameId)
-
-local MainSection = MainTab:CreateSection("你的账号年龄:"..player.AccountAge.."天")
-
-local MainSection = MainTab:CreateSection("你的用户ID:"..game.Players.LocalPlayer.UserId)
-
-local MainSection = MainTab:CreateSection("获取客户端ID:"..game:GetService("RbxAnalyticsService"):GetClientId())
-
-local MainSection = MainTab:CreateSection("你的注入器:"..identifyexecutor())
-
-local MainSection = MainTab:CreateSection("你现在的服务器名称:"..game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name)
-
-local MainSection = MainTab:CreateSection("你的用户名:"..game.Players.LocalPlayer.Character.Name)
+local MainSection = MainTab:CreateSection("你的用户名:"," "..game.Players.LocalPlayer.Name.."")
+local MainSection = MainTab:CreateSection("你的注入器:"," "..identifyexecutor().."")
+local MainSection = MainTab:CreateSection("你所在的服务器ID"," "..game.GameId.."")
+     end
+})
 Rayfield:Notify({
    Title = "冷bing启动",
    Content = "明知不能在一起 却还要假装...",
