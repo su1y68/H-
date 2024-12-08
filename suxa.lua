62826147b7fb3a05f6a6fcb647c403a98c03f6c3
Suxa <1477690040@qq.com>
2024-12-08 12:49:05 +0800

    Update suxa.lua

--- suxa.lua
+++ suxa.lua
@@ -7,27 +7,11 @@ local Window = Rayfield:CreateWindow({
    ConfigurationSaving = {
       Enabled = true,
       FolderName = nil, -- Create a custom folder for your hub/game
-      FileName = "YoutubeRobloxScripts"
+      FileName = "ROBLOX script"
    },
-   Discord = {
-      Enabled = true,
-      Invite = "https://discord.gg/roblox-scripts-942431667807735888", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
-      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
-   },
-   KeySystem = false, -- Set this to true to use our key system
-   KeySettings = {
-      Title = "Nixks Hub | Keyless",
-      Subtitle = "Keyless Hub!",
-      Note = "Join server from misc tab",
-      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
-   }
-})
 
 local MainTab = Window:CreateTab("TSB", nil) -- Title, Image
-local MainSection = MainTab:CreateSection("Main")
+local MainSection = MainTab:CreateSection("下面是攻击类")
 Rayfield:Notify({
    Title = "Script Executed",
    Content = "如果不能再回去 为何还要认识?😔",
