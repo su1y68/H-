c4f0cbfcd7fe8d79c0532d34f73c85dce70c8575
Suxa <1477690040@qq.com>
2024-12-08 22:38:30 +0800

    Update 林.lua

--- 林.lua
+++ 林.lua
@@ -1,4 +1,4 @@
-local OrionLib = loadstring(game:HttpGet("https://pastebin.com/raw/DLDWqkKq"))()
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local LBLG = Instance.new("ScreenGui", getParent)
 local LBL = Instance.new("TextLabel", getParent)
 local player = game.Players.LocalPlayer
