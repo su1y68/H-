73d3bb3cc440157e85e876358c42e789cfbfc475
Suxa <1477690040@qq.com>
2024-12-09 20:07:04 +0800

    su

---
+++ l.lua
@@ -0,0 +1,46 @@
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/Rayfield-UI/Rayfield/main/source'))()
+
+local Window = Rayfield:CreateWindow({
+    Name = "冷bing🧊",
+    LoadingTitle = "bing中心",
+    LoadingSubtitle = "by suxa",
+    Image = "rbxassetid://1818"  -- 替换为你的背景图 Asset ID
+})
+
+local PlayerTab = Window:CreateTab("Player", 4483362458) -- Title, Image
+
+ local Slider = PlayerTab:CreateSlider({
+    Name = "WalkSpeed",
+    Range = {1, 10},
+    Increment = 1,
+    Suffix = "Speed",
+    CurrentValue = 10,
+    Flag = "Slider1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+    Callback = function(Value)
+     game.Players.LocalPlayer.Character:SetAttribute("SpeedMultiplier", Value)
+    end,
+ })
+
+ local Slider = PlayerTab:CreateSlider({
+    Name = "Dash length",
+    Range = {10, 1000},
+    Increment = 1,
+    Suffix = "Length",
+    CurrentValue = 10,
+    Flag = "Slider2", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+    Callback = function(Value)
+     game.Players.LocalPlayer.Character:SetAttribute("DashLength", Value)
+    end,
+ })
+
+ local Slider = PlayerTab:CreateSlider({
+    Name = "Jump Height",
+    Range = {10, 500},
+    Increment = 1,
+    Suffix = "Height",
+    CurrentValue = 10,
+    Flag = "Slider3", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+    Callback = function(Value)
+     game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
+    end,
+ })
