2b4dd80cd47569cdc07db62858e14efee1c1876e
Suxa <1477690040@qq.com>
2024-12-10 22:20:49 +0800

    动作集

---
+++ 副RTF冷脚本.lua
@@ -0,0 +1,33 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+   Name = "冷tsb中心🧊",
+   LoadingTitle = "bing",
+   LoadingSubtitle = "by suxa",
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil, -- Create a custom folder for your hub/game
+      FileName = "suxa NB"
+   },
+   Discord = {
+      Enabled = false,
+      Invite = "sirius", -- The Discord invite code, do not include discord.gg/
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+   },
+   KeySystem = true, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "冷bing tsb动作集",
+      Subtitle = "Key 系统",
+      Note = "请加入QQ群主",
+      FileName = "SiriusKey",
+      SaveKey = true,
+      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = "suxa NB"
+   }
+})
+
+local Tab = Window:CreateTab("饿狼", 4483362458) -- Title, Image
+
+
+
+Rayfield:LoadConfiguration()
