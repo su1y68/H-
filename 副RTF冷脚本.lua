72839dd3a298693cd75b20695f55b7cda7cfdc10
Suxa <1477690040@qq.com>
2024-12-14 00:39:51 +0800

    Update 副RTF冷脚本.lua

--- 副RTF冷脚本.lua
+++ 副RTF冷脚本.lua
@@ -40,8 +40,120 @@ local Window = Rayfield:CreateWindow({
 })
 
 local Tab = Window:CreateTab("动作集类", 4483362458) -- Title, Image
+Section:Set("奇遇专区")
+local Button = Tab:CreateButton({
+   Name = "jjs GOJO",
+   Info = "Button info/Description.",
+   Interact = 'Changable',
+   Callback = function()
+loadstring(game:HttpGet("https://gist.githubusercontent.com/JcBoomin/a63e9ac3e90cef03ecf37e997fd21632/raw/98b567b81f61bb30042e0078b78f3fb24685fb8d/Gojo",true))()
+   end,
+})
+
+Rayfield:Notify({
+	Title = "本bing tsb动作集中心",
+	Content = "by suxa",
+	Duration = 9.5,
+	Image = 4483362458,
+	Actions = { -- Notification Buttons
+		Ignore = {
+			Name = "yy",
+			Callback = function()
+				print("kl")
+			end
+		},
+	},
+})
+
+Section:Set("常见常用gojo")
+local Button = Tab:CreateButton({
+   Name = "GOJO",
+   Info = "Button info/Description.",
+   Interact = 'Changable',
+   Callback = function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/skibiditoiletfan2007/BaldyToSorcerer/refs/heads/main/LatestV2.lua"))()
+   end,
+})
+
+local Button = Tab:CreateButton({
+   Name = "jun六月",
+   Info = "Button info/Description.",
+   Interact = 'Changable',
+   Callback = function()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Kenjihin69/Kenjihin69/refs/heads/main/Tp%20exploit%20saitama%20to%20jun'))()
+   end,
+})
+
+local Button = Tab:CreateButton({
+   Name = "称次",
+   Info = "Button info/Description.",
+   Interact = 'Changable',
+   Callback = function()
+loadstring(game:HttpGet("https://pastebin.com/raw/eEDYWj8p"))()
+   end,
+})
+
+local Paragraph = Tab:CreateParagraph({Title = "饿狼专区", Content = "部分脚本有bug",SectionParent})
+
+local Button = Tab:CreateButton({
+   Name = "贝塔",
+   Info = "Button info/Description.",
+   Interact = 'Changable',
+   Callback = function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/JcBoomin/UEVEGETA/refs/heads/main/Script"))()
+   end,
+})
+
+local Button = Tab:CreateButton({
+   Name = "改索尼克",
+   Info = "Button info/Description.",
+   Interact = 'Changable',
+   Callback = function()
+loadstring(game:HttpGet("https://pastebin.com/raw/7V1mUBtQ"))()
+   end,
+})
+
+local Button = Tab:CreateButton({
+   Name = "(我还是不知道)",
+   Info = "Button info/Description.",
+   Interact = 'Changable',
+   Callback = function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Reapvitalized/TSB/refs/heads/main/ARCAURA.lua"))()
+   end,
+})
+
+local Label = Tab:CreateLabel("刀锋专区",SectionParent)
+
+local Button = Tab:CreateButton({
+   Name = "(我不知道改啥)",
+   Info = "Button info/Description.",
+   Interact = 'Changable',
+   Callback = function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/zyrask/Nexus-Base/main/atomic-blademaster%20to%20sukuna"))()
+   end,
+})
+
+local Label = Tab:CreateLabel("索尼克专区",SectionParent)
+
+local Button = Tab:CreateButton({
+   Name = "Tojo",
+   Info = "Button info/Description.",
+   Interact = 'Changable',
+   Callback = function()
+loadstring(game:HttpGet"https://raw.githubusercontent.com/Wi-sp/Limitless-legacy/refs/heads/main/GUI")()
+   end,
+})
 
 local Tab = Window:CreateTab("攻击类", 4483362458) -- Title, Image
+
+local Button = Tab:CreateButton({
+   Name = "LAT漏洞",
+   Info = "Button info/Description.",
+   Interact = 'Changable',
+   Callback = function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xVicity/GLACIER/main/LATEST.lua"))()
+   end,
+})
 
 local Tab = Window:CreateTab("返回原bing🧊中心", 4483362458) -- Title, Image
 
